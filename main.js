074d41b527ad0c97895094679e50ffeb5415ecf3
Sushanth Kamabathula <sushanth.kamabathula@gmail.com>
2023-03-27 19:18:31 +0530

    Disabled decrease button when count is less than 0

--- main.js
+++ main.js
@@ -1,45 +1,54 @@
 // buttons and counter text
-const counter = document.querySelector("#counter");
-const btns = document.querySelectorAll(".btn");
+const counter = document.querySelector('#counter')
+const btns = document.querySelectorAll('.btn')
 
 // initialize the count variable
-let count = 0;
+let count = 0
 
 btns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const styles = e.currentTarget.classList;
+  btn.addEventListener('click', (e) => {
+    const styles = e.currentTarget.classList
+    const decButton = document.querySelector('.decrease')
 
-    if (styles.contains("increase")) {
-      count++;
-      counter.classList.add("up");
+    if (styles.contains('increase')) {
+      count++
 
-      setTimeout(() => {
-        counter.classList.remove("up");
-      }, 500);
-    } else if (styles.contains("decrease")) {
-      count--;
-      counter.classList.add("down");
+      if (decButton.disabled) decButton.disabled = false
+
+      counter.classList.add('up')
 
       setTimeout(() => {
-        counter.classList.remove("down");
-      }, 500);
+        counter.classList.remove('up')
+      }, 500)
+    } else if (styles.contains('decrease')) {
+      count--
+
+      if (count < 0) decButton.disabled = true
+
+      counter.classList.add('down')
+
+      setTimeout(() => {
+        counter.classList.remove('down')
+      }, 500)
     } else {
-      count = 0;
+      if (decButton.disabled) decButton.disabled = false
+
+      count = 0
     }
 
     if (count > 0) {
-      counter.style.color = "green";
+      counter.style.color = 'green'
     }
 
     if (count < 0) {
-      counter.style.color = "red";
+      counter.style.color = 'red'
     }
 
     if (count === 0) {
-      counter.style.color = "grey";
+      counter.style.color = 'grey'
     }
 
-    counter.textContent = count;
-  });
-});
+    counter.textContent = count
+  })
+})
 
