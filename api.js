8e6e473adcb4227fcc99f1ed351cda9c679c4ca5
atlehjelmseth <atle.hjelmseth@stud.noroff.no>
2023-07-14 13:46:41 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,37 +1,37 @@
-// let username = 'eltprod';
-// let password = 's7D3VM1pnFWPQZ795RAq';
-// let auth = btoa(`${username}:${password}`);
+let username = 'eltprod';
+let password = 's7D3VM1pnFWPQZ795RAq';
+let auth = btoa(`${username}:${password}`);
 
-// const options = { 
+const options = { 
 
-//   method: 'GET', 
+  method: 'GET', 
 
-//   headers: { 
+  headers: { 
 
-//            'Authorization': `Basic ${auth}`
-//    } 
+           'Authorization': `Basic ${auth}`
+   } 
 
-// }; 
+}; 
 
-// const url = "https://hkmarked.nextcom.no/rest-api/public/v2.0/crm-system/orders?locale=eng&offset=127000&limit=200";
-// const proxy = "https://noroffcors.onrender.com/";
+const url = "https://hkmarked.nextcom.no/rest-api/public/v2.0/crm-system/orders?locale=eng&offset=127000&limit=200";
+const proxy = "https://noroffcors.onrender.com/";
 
-// const corsFix = proxy + url;
+const corsFix = proxy + url;
 
 
-// async function nextcomApi() {
+async function nextcomApi() {
   
-//   try {const response = await fetch(corsFix,options);
-//        const resultsSpec = await response.json();
+  try {const response = await fetch(corsFix,options);
+       const resultsSpec = await response.json();
       
       
-//       console.log(resultsSpec)
-//       console.log(resultsSpec.totalCount)
+      console.log(resultsSpec)
+      console.log(resultsSpec.totalCount)
 
-//       }catch (error) {
-//         console.log("error")
-//     }
-//    }
+      }catch (error) {
+        console.log("error")
+    }
+   }
 
-//   nextcomApi()
+  nextcomApi()
   
