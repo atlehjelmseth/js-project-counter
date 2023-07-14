8d5e619094e62cddcd75d3fbf24b5f0e2ddb3b8b
atlehjelmseth <atle.hjelmseth@stud.noroff.no>
2023-07-14 16:00:32 +0200

    Update

--- api.js
+++ api.js
@@ -1,17 +1,22 @@
 let username = 'eltprod';
 let password = 's7D3VM1pnFWPQZ795RAq';
 let auth = btoa(`${username}:${password}`);
+const number = document.querySelector("#counter");
+const interval = setInterval(function() {
+  nextcomApi();
+}, 3000);
 
-const options = { 
 
-  method: 'GET', 
+const options = {
 
-  headers: { 
+  method: 'GET',
+
+  headers: {
 
            'Authorization': `Basic ${auth}`
-   } 
+   }
 
-}; 
+};
 
 const url = "https://hkmarked.nextcom.no/rest-api/public/v2.0/crm-system/orders?locale=eng&offset=127000&limit=200";
 const proxy = "https://noroffcors.onrender.com/";
@@ -20,18 +25,20 @@ const corsFix = proxy + url;
 
 
 async function nextcomApi() {
-  
+
   try {const response = await fetch(corsFix,options);
        const resultsSpec = await response.json();
-      
-      
-      console.log(resultsSpec)
+
+
+      // console.log(resultsSpec)
       console.log(resultsSpec.totalCount)
+      number.innerHTML = -'127542' + resultsSpec.totalCount;
 
       }catch (error) {
         console.log("error")
     }
+    // setTimeout(nextcomApi(), 15000);
    }
 
   nextcomApi()
-  
+
